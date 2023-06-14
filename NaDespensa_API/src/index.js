5553fac4d6ff74c1e8d26e0d90bb3c057cdb8d4d
DouglasGPSalgado <douglasgsk8@gmail.com>
2023-06-13 22:21:25 -0300

    feat: :sparkles: Autenticação JWT

--- NaDespensa_API/src/index.js
+++ NaDespensa_API/src/index.js
@@ -1,28 +1,38 @@
-const express = require('express');
- mongoose = require('mongoose');
+//imports
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
-
 // Conexão com o banco de dados MongoDB
-mongoose.connect('mongodb://localhost/estoque_de_alimentos', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conexão com o MongoDB estabelecida com sucesso!'))
-  .catch(err => console.log('Erro ao conectar com o MongoDB: ', err));
+// mongoose
+//   .connect("mongodb://localhost/estoque_de_alimentos", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Conexão com o MongoDB estabelecida com sucesso!"))
+//   .catch((err) => console.log("Erro ao conectar com o MongoDB: ", err));
 
 // Definição do schema do alimento
 const foodSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   quantidade: { type: Number, required: true },
-  dataDeValidade: { type: Date, required: true }
+  dataDeValidade: { type: Date, required: true },
 });
 
 // Definição do modelo do alimento
-const Food = mongoose.model('Food', foodSchema);
+const Food = mongoose.model("Food", foodSchema);
 
 // Middleware para permitir o uso de JSON no corpo das requisições
 app.use(express.json());
 
+//Models
+const User = require("./models/User");
+
 // Endpoint para criar um novo alimento
-app.post('/foods', async (req, res) => {
+app.post("/foods", async (req, res) => {
   try {
     const food = new Food(req.body);
     await food.save();
@@ -33,9 +43,11 @@ app.post('/foods', async (req, res) => {
 });
 
 // Endpoint para atualizar um alimento existente
-app.put('/foods/:id', async (req, res) => {
+app.put("/foods/:id", async (req, res) => {
   try {
-    const food = await Food.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const food = await Food.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     if (!food) {
       return res.status(404).send();
     }
@@ -46,7 +58,7 @@ app.put('/foods/:id', async (req, res) => {
 });
 
 // Endpoint para excluir um alimento
-app.delete('/foods/:id', async (req, res) => {
+app.delete("/foods/:id", async (req, res) => {
   try {
     const food = await Food.findByIdAndDelete(req.params.id);
     if (!food) {
@@ -59,7 +71,7 @@ app.delete('/foods/:id', async (req, res) => {
 });
 
 // Endpoint para listar todos os alimentos
-app.get('/foods', async (req, res) => {
+app.get("/foods", async (req, res) => {
   try {
     const foods = await Food.find();
     res.send(foods);
@@ -69,7 +81,7 @@ app.get('/foods', async (req, res) => {
 });
 
 // Endpoint para buscar um alimento por ID
-app.get('/foods/:id', async (req, res) => {
+app.get("/foods/:id", async (req, res) => {
   try {
     const food = await Food.findById(req.params.id);
     if (!food) {
@@ -82,16 +94,164 @@ app.get('/foods/:id', async (req, res) => {
 });
 
 // Endpoint para buscar alimentos por nome
-app.get('/foods', async (req, res) => {
+app.get("/foods", async (req, res) => {
   try {
-    const foods = await Food.find({ nome: req.query.nome, quantidade: { $gt: 0 }, dataDeValidade: { $gte: new Date() } });
+    const foods = await Food.find({
+      nome: req.query.nome,
+      quantidade: { $gt: 0 },
+      dataDeValidade: { $gte: new Date() },
+    });
     res.send(foods);
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
-// Inicialização do servidor
-app.listen(3000, () => {
-  console.log('Servidor iniciado na porta 3000');
+// DASDSADSADASDASDSADASDSADSADASDASDASDASDASDASDASDSADASDASDASDASDSADSADSAADSADASDSADSADASDSDAS
+
+//Rota privada
+app.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  //Checar se usuário existe
+  const user = await User.findById(id, "-password");
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado" });
+  }
+
+  res.status(200).json({ user });
 });
+
+function checkToken(req, res, next){
+
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(" ")[1]
+
+  if(!token){ 
+    return res.status(401).json({msg: 'Acesso negado!'})
+  }
+  try{
+    const secret = process.env.SECRET
+
+    jwt.verify(token, secret)
+    
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "Token inválido!",
+    });
+  }
+}
+
+//Registrar Usuário
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmpassword } = req.body;
+
+  //validações
+  if (!name) {
+    return res.status(422).json({ msg: "O nome é obrigatório!" });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória!" });
+  }
+
+  if (password !== confirmpassword) {
+    return res.status(422).json({ msg: "As senhas não conferem!" });
+  }
+  //Checar se um usuario existe
+  const UserExists = await User.findOne({ email: email });
+
+  if (UserExists) {
+    return res.status(422).json({ msg: "Por favor, utilize outro e-mail!" });
+  }
+
+  //Criar senha
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  try {
+    await user.save();
+
+    res.status(201).json({ msg: "Usuario criado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Aconteceu um erro no servidor, tente novamente mais tarde",
+    });
+  }
+});
+
+//Logind de Usuário
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  //Validações
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória!" });
+  }
+
+  //Ver se o usuario existe
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(422).json({ msg: "Usuário não encontrado!" });
+  }
+
+  //Ver se a senha bate
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida!" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+
+    res.status(200).json({ msg: "Autenticação realizada com sucesso", token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Aconteceu um erro no servidor, tente novamente mais tarde",
+    });
+  }
+});
+
+//Credenciais
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+// Inicialização do servidor
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.amk28ys.mongodb.net/AuthNaDespensa?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Conectou ao banco!");
+    });
+  })
+  .catch((err) => console.log(err));
